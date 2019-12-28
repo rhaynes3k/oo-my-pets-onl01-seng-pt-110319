c502a742c5f669cd54a99c2965fcd92877792041
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-28 13:59:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,36 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_accessor :cats
+  attr_reader :species
+  @@all = []
+  def initialize(name,species="human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+  
+  def name
+    @name
+  end
+  
+  def say_species
+    "I am a human."
+    #binding.pry
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def cats
+    Cats.all.select{|c|c.owner == self}
+  end
 end
