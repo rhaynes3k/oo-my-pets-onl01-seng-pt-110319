0b72cdbab78c5c7b35343a3d73212a258ebb4088
Rommel Haynes <rhaynes3k@gmail.com>
2019-12-28 16:07:37 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,41 @@
 class Owner
-  # code goes here
+  attr_accessor :cats
+  attr_reader :species
+  @@all = []
+  def initialize(name,species="human")
+    @name = name
+    @species = species
+    @@all << self
+  end
+  
+  def name
+    @name
+  end
+  
+  def say_species
+    "I am a human."
+    #binding.pry
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.size
+  end
+  
+  def self.reset_all
+    @@all = []
+  end
+  
+  def cats
+    Cat.all.select{|c|c.owner.name == self.name}
+    #binding pry
+  end
+  
+  def dogs
+    Dog.all.select{|d|d.owner.name == self.name}
+    #binding pry
+  end
 end
